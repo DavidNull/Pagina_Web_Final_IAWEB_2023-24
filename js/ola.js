762fb02063433a5061674259eed93082cfbbe8e1
David Cela <156198058+DavidNull@users.noreply.github.com>
2024-05-10 14:50:21 +0200

    Add files via upload

---
+++ js/ola.js
@@ -0,0 +1,17 @@
+const header = document.getElementById('header');
+
+header.addEventListener('mousemove', function(e) {
+  const wave = document.createElement('div');
+  wave.classList.add('wave');
+  header.appendChild(wave);
+
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  wave.style.left = mouseX + 'px';
+  wave.style.top = mouseY + 'px';
+
+  setTimeout(() => {
+    wave.remove();
+  }, 1000); 
+});
